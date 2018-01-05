4f0f205a6806c72def35a724fe7cf3014e74e998
gschora <hannes.dummy@putz-transporte.at>
2018-01-05 01:13:15 +0100

    lf test

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -9,7 +9,7 @@
       <v-list dense>
         <v-list-tile @click="">
           <v-list-tile-action>
-            <!-- <v-icon>dashboard</v-icon> -->
+            <v-icon>dashboard</v-icon>
           </v-list-tile-action>
           <v-list-tile-content>
             <v-list-tile-title>Dashboard</v-list-tile-title>
