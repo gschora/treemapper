fd7095fe18d2c68862b4bf7b952c5547f1f88d5b
gschora <hannes.dummy@putz-transporte.at>
2018-01-09 23:28:38 +0100

    openlayers finally showing a map :-D

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -7,7 +7,7 @@
       app
     >
     <v-list dense three-line>
-        <v-list-tile @click="">
+        <v-list-tile @click.stop="drawer = !drawer">
           <v-list-tile-action>
             <v-icon>map</v-icon>
           </v-list-tile-action>
@@ -15,7 +15,7 @@
             <v-list-tile-title>Karte</v-list-tile-title>
           </v-list-tile-content>
         </v-list-tile>
-        <v-list-tile @click="">
+        <v-list-tile @click.stop="drawer = !drawer">
           <v-list-tile-action>
             <v-icon>settings</v-icon>
           </v-list-tile-action>
--- src/renderer/components/MapPage/MapPage.vue
+++ src/renderer/components/MapPage/MapPage.vue
@@ -1,14 +1,52 @@
 <template>
-  <div>
-    <h1>MapPage</h1>
+  <div id="mapdiv">
+    <!-- <h1>MapPage</h1> -->
   </div>
 </template>
 
 <script>
+  import OLMap from '../../../../node_modules/ol/map';
+  import OLTile from '../../../../node_modules/ol/layer/tile';
+  import OLOSM from '../../../../node_modules/ol/source/osm';
+  import OLView from '../../../../node_modules/ol/view';
+  import('../../../../node_modules/ol/ol.css');
+
+function initMap() {
+    const myMap = new OLMap({
+      layers: [
+        new OLTile({
+          source: new OLOSM(),
+        }),
+      ],
+      target: 'mapdiv',
+      view: new OLView({
+        center: [0, 0],
+        zoom: 2,
+      }),
+    });
+
+    myMap.render();
+}
+
   export default {
+    name: 'MapPage',
+    data() {
+      return {
+        msg: 'null value',
+      };
+    },
+    // mounted ist WICHTIG!!!!!! Sonst wird map zu früh gerendert und nicht angezeigt!!!!!!
+    mounted() {
+      this.$nextTick(() => {
+        initMap();
+      });
+    },
   };
 </script>
 
 <style>
-
+  #mapdiv {
+    width: 100%;
+    height: 100%;
+  }
 </style>
