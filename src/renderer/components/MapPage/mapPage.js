2bc0093818a09a9e46c4823d36e56fdd2d2ab498
gschora <hannes.dummy@putz-transporte.at>
2018-01-11 15:39:04 +0100

    leaflet working with maps from basemap.at, openstreetmap layerswitching working

--- src/renderer/components/MapPage/mapPage.js
+++ src/renderer/components/MapPage/mapPage.js
@@ -1,14 +1,44 @@
 import L from 'leaflet';
 import('leaflet/dist/leaflet.css');
 
-export default function initMap() {
-  const mymap = L.map('mapdiv').setView([51.505, -0.09], 13);
-  mymap.setZoom(2);
+function basemapLayers(mapStyle, imgType) {
+  const basemapUrl = `http://{s}.wien.gv.at/basemap/${mapStyle}/normal/google3857/{z}/{y}/{x}.${imgType}`;
+  const basemapAttrib = 'Map data &copy; <a href="https://www.basemap.at">basemap.at</a>';
+  return L.tileLayer(basemapUrl, {
+    type: 'map',
+    subdomains: ['maps', 'maps1', 'maps2', 'maps3', 'maps4'],
+    attribution: basemapAttrib,
+  });
+}
 
-  L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution:
-      'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
+function osmap() {
+  // Attribution OSM-MAP from tile.osm.org wmts-server
+  const osm = 'http://{s}.tile.osm.org/{z}/{x}/{y}.png';
+  const osmAttr =
+    '&copy; <a href="http://osm.org/copyright" target="_blank">OpenStreetMap</a> contributors';
+  return L.tileLayer(osm, {
+    type: 'map',
+    attribution: osmAttr,
+    zIndex: 1,
     maxZoom: 18,
-    minZoom: 2,
-  }).addTo(mymap);
+  });
+}
+
+const mapLayers = {
+  'basemap.at Color': basemapLayers('geolandbasemap', 'png'),
+  'basemap.at Grau': basemapLayers('bmapgrau', 'png'),
+  'basemap.at Orthophoto': basemapLayers('bmaporthofoto30cm', 'jpeg'),
+  OpenStreetMap: osmap(),
+};
+
+export default function initMap() {
+  const mymap = L.map('mapdiv', {
+    crs: L.CRS.EPSG3857,
+  });
+  mymap.setView([47.34, 15.83], 7);
+
+  mymap.addLayer(mapLayers['basemap.at Color']);
+
+  const layerCtrl = L.control.layers(mapLayers);
+  mymap.addControl(layerCtrl);
 }
