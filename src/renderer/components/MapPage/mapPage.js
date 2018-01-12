c31589ff6b741de652c3e54b31d8ac7bbbaf514b
gschora <hannes.dummy@putz-transporte.at>
2018-01-12 03:18:05 +0100

    german translation for map edit buttons

--- src/renderer/components/MapPage/mapPage.js
+++ src/renderer/components/MapPage/mapPage.js
@@ -36,6 +36,39 @@ function googleMaps() {
   });
 }
 
+function setDEText() {
+  L.drawLocal.draw.toolbar.buttons.polygon = 'Fläche';
+  L.drawLocal.draw.toolbar.buttons.polyline = 'Strecke';
+  L.drawLocal.draw.toolbar.finish.title = 'Zeichnung speichern';
+  L.drawLocal.draw.toolbar.finish.text = 'speichern';
+  L.drawLocal.draw.toolbar.undo.title = 'den letzten Punk löschen';
+  L.drawLocal.draw.toolbar.undo.text = 'rückgängig';
+  L.drawLocal.draw.toolbar.actions.title = 'Zeichnen abbrechen';
+  L.drawLocal.draw.toolbar.actions.text = 'abbrechen';
+  L.drawLocal.draw.handlers.polygon.tooltip.start = 'zum Starten auf die Karte klicken...';
+  L.drawLocal.draw.handlers.polygon.tooltip.cont = '';
+  L.drawLocal.draw.handlers.polygon.tooltip.end = 'zum Schließen auf den ersten Punkt klicken...';
+  L.drawLocal.draw.handlers.polyline.tooltip.start = 'zum Starten auf die Karte klicken...';
+  L.drawLocal.draw.handlers.polyline.tooltip.cont = '';
+  L.drawLocal.draw.handlers.polyline.tooltip.end = 'zum Beenden auf den letzten Punkt klicken...';
+  L.drawLocal.draw.handlers.polyline.error =
+    '<strong>Fehler:</strong> Linien dürfen sich nicht überschneiden!';
+  L.drawLocal.edit.toolbar.actions.save.title = 'Änderungen speichern';
+  L.drawLocal.edit.toolbar.actions.save.text = 'speichern';
+  L.drawLocal.edit.toolbar.actions.cancel.title = 'Änderungen verwerfen';
+  L.drawLocal.edit.toolbar.actions.cancel.text = 'abbrechen';
+  L.drawLocal.edit.toolbar.actions.clearAll.title = 'ACHTUNG!!! alle Zeichnungen löschen...';
+  L.drawLocal.edit.toolbar.actions.clearAll.text = 'Alles löschen';
+  L.drawLocal.edit.toolbar.buttons.edit = 'editieren';
+  L.drawLocal.edit.toolbar.buttons.editDisabled = 'keine Zeichnungen zum Editieren...';
+  L.drawLocal.edit.toolbar.buttons.remove = 'löschen';
+  L.drawLocal.edit.toolbar.buttons.removeDisabled = 'keine Zeichnungen zum Löschen...';
+  L.drawLocal.edit.handlers.edit.tooltip.text = 'zum Editieren die Kästchen ziehen...';
+  L.drawLocal.edit.handlers.edit.tooltip.subtext =
+    'auf "abbrechen" klicken um die Änderungen zu verwerfen...';
+  L.drawLocal.edit.handlers.remove.tooltip.text = 'zum Löschen auf eine Zeichnung klicken...';
+}
+
 const mapLayers = {
   'basemap.at Color': basemapLayers('geolandbasemap', 'png'),
   'basemap.at Grau': basemapLayers('bmapgrau', 'png'),
@@ -45,6 +78,7 @@ const mapLayers = {
 };
 
 function setupMapCtrl() {
+  setDEText();
   const layerCtrl = L.control.layers(mapLayers);
   mymap.addControl(layerCtrl);
 
