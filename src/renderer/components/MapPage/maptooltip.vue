271fe148d838ad9bed251da4510792b6193b4e7f
gschora <hannes.dummy@putz-transporte.at>
2018-01-27 13:28:19 +0100

    renamed tooltip to maptooltip tooltip has now buttons for saving home position and for adding address location

---
+++ src/renderer/components/MapPage/maptooltip.vue
@@ -0,0 +1,62 @@
+<template>
+    <div v-show='{show}' :id='ttpDivid' :class="cssClass">
+        <div>
+            <span :id='ttpLabelid'>{{labeltext}}</span>
+        </div>
+        <div>
+          <v-tooltip bottom>
+            <v-btn fab small color='teal darken-3' slot="activator">
+              <v-icon dark>mdi-home-map-marker</v-icon>
+            </v-btn>
+            <label>als Heimatpunkt speichern</label>
+          </v-tooltip>
+          <v-tooltip bottom>
+            <v-btn fab small color='light-blue darken-4' slot="activator">
+              <v-icon dark>add</v-icon>
+            </v-btn>
+            <label>Adresse speichern</label>
+          </v-tooltip>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  props: {
+    ttpLabelid: {
+      // changes label id
+      type: String,
+      default: 'tooltptxt',
+    },
+    ttpDivid: {
+      // changes outer div id
+      type: String,
+      default: 'tooltpdiv',
+    },
+    cssClass: {
+      // css classes to use
+      type: Array,
+      default: () => ['mtooltip', 'mtooltip-static'],
+    },
+    labeltext: {
+      type: String,
+      default: "I'm a tooltip",
+    },
+    show: true,
+  },
+  data() {
+    return {
+      //   show: true, // display:none
+      //   labeltext: 'toltiptext', // default text of label
+    };
+  },
+  methods: {},
+};
+</script>
+
+<style>
+button.btn.btn--floating {
+  width: 25px;
+  height: 25px;
+}
+</style>
